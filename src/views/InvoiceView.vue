4c5a4303cda687a158d9cd8686cee7c03d93f261
Kenno Pajunurm <33248162+Stotex@users.noreply.github.com>
2021-07-15 14:33:09 +0300

    Added edit invoice function

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -14,7 +14,7 @@
                 </div>
             </div>
             <div class="right flex">
-                <button @click="toggleEditInvoice(currentInvoice.docId)" class="dark-purple">Edit</button>
+                <button @click="toggleEditInvoice" class="dark-purple">Edit</button>
                 <button @click="deleteInvoice(currentInvoice.docId)" class="red">Delete</button>
                 <button v-if="currentInvoice.invoicePending" @click="updateStatusToPaid(currentInvoice.docId)"
                         class="green">Mark as Paid
@@ -97,15 +97,27 @@ export default {
         this.getCurrentInvoice();
     },
     methods: {
-        ...mapMutations(["SET_CURRENT_INVOICE"]),
+        ...mapMutations(["SET_CURRENT_INVOICE", "TOGGLE_EDIT_INVOICE", "TOGGLE_INVOICE"]),
 
         getCurrentInvoice() {
             this.SET_CURRENT_INVOICE(this.$route.params.invoiceId);
             this.currentInvoice = this.currentInvoiceArray[0];
+        },
+
+        toggleEditInvoice() {
+            this.TOGGLE_EDIT_INVOICE();
+            this.TOGGLE_INVOICE();
         }
     },
     computed: {
-        ...mapState(["currentInvoiceArray"])
+        ...mapState(["currentInvoiceArray", "editInvoice"])
+    },
+    watch: {
+        editInvoice() {
+            if(!this.editInvoice){
+                this.currentInvoice = this.currentInvoiceArray[0];
+            }
+        }
     }
 }
 </script>
