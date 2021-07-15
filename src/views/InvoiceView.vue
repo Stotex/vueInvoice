2e20bbeaaaf7ac97cc5338bd8dca7cb4088345a1
Kenno Pajunurm <33248162+Stotex@users.noreply.github.com>
2021-07-15 14:38:58 +0300

    Added deleting function

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -84,7 +84,7 @@
 </template>
 
 <script>
-import {mapMutations, mapState} from "vuex";
+import {mapActions, mapMutations, mapState} from "vuex";
 
 export default {
     name: "InvoiceView",
@@ -99,6 +99,8 @@ export default {
     methods: {
         ...mapMutations(["SET_CURRENT_INVOICE", "TOGGLE_EDIT_INVOICE", "TOGGLE_INVOICE"]),
 
+        ...mapActions(['DELETE_INVOICE']),
+
         getCurrentInvoice() {
             this.SET_CURRENT_INVOICE(this.$route.params.invoiceId);
             this.currentInvoice = this.currentInvoiceArray[0];
@@ -107,6 +109,11 @@ export default {
         toggleEditInvoice() {
             this.TOGGLE_EDIT_INVOICE();
             this.TOGGLE_INVOICE();
+        },
+
+        async deleteInvoice(docId) {
+            await this.DELETE_INVOICE(docId);
+            this.$router.push({name: "Home"});
         }
     },
     computed: {
